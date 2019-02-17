30cf562725b228d4a7f57840fa53b00003bd495b
gauravaditya <gauravaditya3@gmail.com>
2019-02-17 23:26:54 +0530

    Updated DB configuration for Heroku deployment

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {User} = require('./models/user');
 
 const app = express();
 
+const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -50,7 +51,7 @@ app.get('/todos/:id', (req, res) => {
 
 })
 
-app.listen(3000, () =>{
+app.listen(`${port}`, () =>{
     console.log('Server started on port 3000');
 });
 
